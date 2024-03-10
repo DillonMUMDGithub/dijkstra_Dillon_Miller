50c412ed6c71a14238d80b3f97aee831d69820b6
DillonMUMDGithub <dillon.miller2002@gmail.com>
2024-03-10 19:25:52 -0400

    Update Project2_Main.py
    Updated Main file, added code from ObstacleMatTest into the main file, implemented color to the map and to the visualization

--- Project2_Main.py
+++ Project2_Main.py
@@ -2,7 +2,7 @@
     """
     
 #Establish Obstacle Matrix    
-from ObstacleMatTest import ObstMat
+# from ObstacleMatTest import ObstMat
 import matplotlib.pyplot as plt
 import time as time
 from queue import PriorityQueue
@@ -102,6 +102,200 @@ def CheckAllClosed(Nodes,Closed):
             InClosedMat[7] = 1    
     
     return InClosedMat
+ObstMat = np.full((500,1200),np.inf)
+ObstMatR = np.full((500,1200),0)
+ObstMatG = np.full((500,1200),0)
+ObstMatB = np.full((500,1200),0)
+
+
+def normalize(im):
+   # Normalise image to range 0..1
+    min, max = im.min(), im.max()
+    return (im.astype(float)-min)/(max-min)
+# ObstMat[]
+# print(ObstMat[500][0])
+print(ObstMat.shape)
+for i in range(94,180):
+    for k in range(89,500):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 255
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 0
+        
+for i in range(99,175):
+    for k in range(94,500):
+        ObstMat[k][i] = -1
+        ObstMatB[k][i] = 255
+        ObstMatR[k][i] = 0
+        
+# print(ObstMat[150][300])
+for i in range(269,354):
+    for k in range(0,404):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 255
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 0
+for i in range(274,349):
+    for k in range(0,399):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 0
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 255        
+        
+#right obstacle
+
+for i in range(1014,1104):
+    for k in range(44,454):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 255
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 0
+for i in range(1019,1099):
+    for k in range(49,449):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 0
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 255
+for i in range(894,1019):
+    for k in range(369,454):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 255
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 0
+for i in range(899,1019):
+    for k in range(374,449):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 0
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 255
+        
+for i in range(894,1019):
+    for k in range(44,124):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 255
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 0
+for i in range(899,1019):
+    for k in range(49,119):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 0
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 255        
+        
+#Hexagon
+
+#MiddleRectangle:
+for i in range(514,784):
+    for k in range(174,324):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 255
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 0
+        #Red Barrier Middle Rectangle
+for i in range(519,779):
+    for k in range(174,324):
+        ObstMat[k][i] = -1
+        ObstMatR[k][i] = 0
+        ObstMatG[k][i] = 0
+        ObstMatB[k][i] = 255       
+        
+#TopLeftTriangle
+GRat = 80/135
+for i in range(514,649):
+    for k in range(324,404):
+        # ObstMat[k][i] = -1
+        XTemp = i-514
+        YTemp = k-324
+        if (XTemp*GRat)>YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 255
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 0 
+#Tope Left No Barrier
+GRat = 75/130
+for i in range(519,649):
+    for k in range(324,399):
+        # ObstMat[k][i] = -1
+        XTemp = i-519
+        YTemp = k-324
+        if (XTemp*GRat)>YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 0
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 255            
+            
+            
+#TopRight Triangle
+GRat = -80/135          
+for i in range(649,784):
+    for k in range(324,404):
+        # ObstMat[k][i] = -1
+        XTemp = i-649
+        YTemp = k-324
+        if (XTemp*GRat)+80>YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 255
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 0 
+            #TopRight No barrier
+GRat = -75/130        
+for i in range(649,779):
+    for k in range(324,399):
+        # ObstMat[k][i] = -1
+        XTemp = i-649
+        YTemp = k-324
+        if (XTemp*GRat)+75>YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 0
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 255        #Need to fix  
+#BotRightTriangle
+GRat = 80/135
+for i in range(649,784):
+    for k in range(94,174):
+        # ObstMat[k][i] = -1
+        XTemp = i-649
+        YTemp = k-94
+        if (XTemp*GRat)<YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 255
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 0 
+GRat = 75/130
+for i in range(649,779):
+    for k in range(99,174):
+        # ObstMat[k][i] = -1
+        XTemp = i-649
+        YTemp = k-99
+        if (XTemp*GRat)<YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 0
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 255 
+#BotLeft Triangle
+GRat = -80/135          
+for i in range(514,649):
+    for k in range(94,174):
+        # ObstMat[k][i] = -1
+        XTemp = i-514
+        YTemp = k-94
+        if (XTemp*GRat)+80<YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 255
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 0     
+GRat = -75/130        
+for i in range(519,649):
+    for k in range(99,174):
+        # ObstMat[k][i] = -1
+        XTemp = i-519
+        YTemp = k-99
+        if (XTemp*GRat)+75<YTemp:
+            ObstMat[k][i] = -1
+            ObstMatR[k][i] = 0
+            ObstMatG[k][i] = 0
+            ObstMatB[k][i] = 255
+
 
 startNode = [0,0]
 endNode = [1150,250]
@@ -123,6 +317,7 @@ TotalQ = PriorityQueue()
 TotalQ.put((0, [startNode,'N/A']))
 ClosedQPrio = PriorityQueue()
 
+imgmat = []
 Parent.append('N/A')
 Go = 1
 
@@ -171,7 +366,7 @@ while(Stop==0):
             if B ==0:
                 if UpC2C<ObstMat[Up[1]][Up[0]]:
                     ObstMat[Up[1]][Up[0]] = UpC2C
-                    
+                    ObstMatG[Up[1]][Up[0]] = 255
                     OpenQ.append(Up)
                     Parent.append(WorkingNode)
                     C2C.append(UpC2C)
@@ -185,7 +380,7 @@ while(Stop==0):
             if B ==0:
                 if DownC2C<ObstMat[Down[1]][Down[0]]:
                     ObstMat[Down[1]][Down[0]] = DownC2C
-                    
+                    ObstMatG[Down[1]][Down[0]] = 255
                     OpenQ.append(Down)
                     Parent.append(WorkingNode)
                     C2C.append(DownC2C)
@@ -198,7 +393,7 @@ while(Stop==0):
             if B ==0:
                 if LeftC2C<ObstMat[Left[1]][Left[0]]:
                     ObstMat[Left[1]][Left[0]] = LeftC2C
-                    
+                    ObstMatG[Left[1]][Left[0]] = 255
                     OpenQ.append(Left)
                     Parent.append(WorkingNode)
                     C2C.append(LeftC2C)
@@ -211,7 +406,7 @@ while(Stop==0):
             if B ==0:
                 if RightC2C<ObstMat[Right[1]][Right[0]]:
                     ObstMat[Right[1]][Right[0]] = RightC2C
-                    
+                    ObstMatG[Right[1]][Right[0]] = 255
                     OpenQ.append(Right)
                     Parent.append(WorkingNode)
                     C2C.append(RightC2C)
@@ -224,7 +419,7 @@ while(Stop==0):
             if B ==0:
                 if ULC2C<ObstMat[UL[1]][UL[0]]:
                     ObstMat[UL[1]][UL[0]] = ULC2C
-                    
+                    ObstMatG[UL[1]][UL[0]] = 255
                     OpenQ.append(UL)
                     Parent.append(WorkingNode)
                     C2C.append(ULC2C)
@@ -237,7 +432,7 @@ while(Stop==0):
             if B ==0:
                 if URC2C<ObstMat[UR[1]][UR[0]]:
                     ObstMat[UR[1]][UR[0]] = URC2C
-                    
+                    ObstMatG[UR[1]][UR[0]] = 255
                     OpenQ.append(UR)
                     Parent.append(WorkingNode)
                     C2C.append(URC2C)
@@ -250,7 +445,7 @@ while(Stop==0):
             if B ==0:
                 if DLC2C<ObstMat[DL[1]][DL[0]]:
                     ObstMat[DL[1]][DL[0]] = DLC2C
-                    
+                    ObstMatG[DL[1]][DL[0]] = 255
                     OpenQ.append(DL)
                     Parent.append(WorkingNode)
                     C2C.append(DLC2C)
@@ -268,6 +463,8 @@ while(Stop==0):
                     Parent.append(WorkingNode)
                     C2C.append(DRC2C)
                     TotalQ.put((DRC2C,[DR,WorkingNode])) 
+
+        
     except:
         break
 
@@ -321,17 +518,34 @@ for pixel in TrackBack:
         ObstMat[pixel[1]][pixel[0]-1] = -1
         ObstMat[pixel[1]][pixel[0]+1] = -1
         ObstMatR[pixel[1]][pixel[0]] = -1
-
+        ObstMatR[pixel[1]-1][pixel[0]] = 255
+        ObstMatR[pixel[1]+1][pixel[0]] = 255
+        ObstMatR[pixel[1]][pixel[0]-1] = 255
+        ObstMatR[pixel[1]][pixel[0]+1] = 255
+        ObstMatG[pixel[1]][pixel[0]] = 255
+        ObstMatG[pixel[1]-1][pixel[0]] = 255
+        ObstMatG[pixel[1]+1][pixel[0]] = 255
+        ObstMatG[pixel[1]][pixel[0]-1] = 255
+        ObstMatG[pixel[1]][pixel[0]+1] = 255
+        ObstMatB[pixel[1]][pixel[0]] = 255
+        ObstMatB[pixel[1]-1][pixel[0]] = 255
+        ObstMatB[pixel[1]+1][pixel[0]] = 255
+        ObstMatB[pixel[1]][pixel[0]-1] = 255
+        ObstMatB[pixel[1]][pixel[0]+1] = 255
         counttrack = counttrack+1
- 
+        #Append the trackback iamges to the 
+        
     except:
         break
     
-   
+ 
 print(len(ClosedQ))
 print(len(ClosedC2C))
 print(len(ClosedParent))
-
+ObstMat3d = np.dstack((ObstMatR,ObstMatG,ObstMatB))
+cv.imwrite("FinalMap.png", ObstMat3d)
+plt.matshow(ObstMat3d)
+plt.show()
 
 
 plt.matshow(ObstMat,cmap = 'gray')
